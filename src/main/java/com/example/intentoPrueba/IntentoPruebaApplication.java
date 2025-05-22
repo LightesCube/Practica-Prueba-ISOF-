188939586d25e4782977437d9a09ea47ab7a480c
Lights <d.marillan02@ufromail.cl>
2025-05-22 03:04:40 -0400

    Proyecto base Spring Boot

---
+++ src/main/java/com/example/intentoPrueba/IntentoPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.example.intentoPrueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntentoPruebaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntentoPruebaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/intentoPrueba/IntentoPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.intentoPrueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntentoPruebaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
